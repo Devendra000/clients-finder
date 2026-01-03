0695e8e89fd34b7e4ecd4b3db4393d4d5878d0ba
Devendra Hamal <devendra.hamal058@gmail.com>
2026-01-03 16:42:00 +0545

    refactor: clean up prisma adapter code by removing unused imports and logging

--- prisma/prisma-adapter.ts
+++ prisma/prisma-adapter.ts
@@ -1,10 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { log } from "console";
-import {config} from 'dotenv';
 
-config();
-log('Using DATABASE_URL:', process.env.DATABASE_URL);
 export const prismaPgAdapter = new PrismaPg({
-  connectionString:
-    process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL
 });
